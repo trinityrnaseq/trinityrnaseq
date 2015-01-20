9968bb9a9260a50fcaa9b194802c42705381bfeb
Brian Haas <brianjohnhaas@gmail.com>
2015-01-20 16:16:01 -0500

    adding splice site info

--- util/misc/run_GSNAP.pl
+++ util/misc/run_GSNAP.pl
@@ -22,6 +22,7 @@ my $usage = <<__EOUSAGE__;
 #  Optional:
 #  -N <int>                    number of top hits (default: 1)
 #  -I <int>                    max intron length (default: 500000)
+#  -G <string>                 GTF file for incorporating reference splice site info.
 #  --CPU <int>                 number of threads (default: 2)
 #  --out_prefix <string>       output prefix (default: gsnap)
 #
@@ -42,6 +43,8 @@ my $help_flag;
 
 my $num_top_hits = 1;
 my $out_prefix = "gsnap";
+my $gtf_file;
+
 
 &GetOptions( 'h' => \$help_flag,
              'genome=s' => \$genome,
@@ -50,6 +53,7 @@ my $out_prefix = "gsnap";
              'CPU=i' => \$CPU,
              'N=i' => \$num_top_hits,
              'out_prefix=s' => \$out_prefix,
+             'G=s' => \$gtf_file,
              );
 
 
@@ -73,9 +77,26 @@ main: {
 		&process_cmd($cmd);
 	}
 
+    my $splice_file;
+    my $splice_param = "";
+    
+    if ($gtf_file) {
+        $splice_file = "$gtf_file.gsnap.splice";
+        if (! -s $splice_file) {
+            # create one.
+            my $cmd = "gtf_splicesites < $gtf_file > $splice_file";
+            &process_cmd($cmd);
+
+            $cmd = "iit_store -o $splice_file.iit < $splice_file";
+        }
+    
+        $splice_param = "--use-splicing=$splice_file.iit";
+    }
+    
+
     ## run GMAP
     
-    my $cmd = "gsnap -D $genomeBaseDir -d $genomeDir -A sam -N 1 -w $max_intron -n $num_top_hits -t $CPU $reads | samtools view -bS - | samtools sort -@ $CPU - $out_prefix.cSorted";
+    my $cmd = "gsnap -D $genomeBaseDir -d $genomeDir -A sam -N 1 -w $max_intron -n $num_top_hits -t $CPU $reads $splice_param | samtools view -bS - | samtools sort -@ $CPU - $out_prefix.cSorted";
     &process_cmd($cmd);
     
     

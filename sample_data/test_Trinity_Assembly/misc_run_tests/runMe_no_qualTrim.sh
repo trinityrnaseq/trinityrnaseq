378bba3b1557f8e6edeb2e8381dfd156082a775b
Brian Haas <brianjohnhaas@gmail.com>
2015-01-22 08:23:09 -0500

    prep for v2.0.0 release

--- sample_data/test_Trinity_Assembly/misc_run_tests/runMe_no_qualTrim.sh
+++ sample_data/test_Trinity_Assembly/misc_run_tests/runMe_no_qualTrim.sh
@@ -7,7 +7,7 @@
 #######################################################
 
 ## use jellyfish
-../../Trinity --seqType fq --JM 2G --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --normalize_reads --no_cleanup -o trin_test_no_qtrim
+../../Trinity --seqType fq --max_memory 2G --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --normalize_reads --no_cleanup --output trinity_test_no_qtrim
 
 ##### Done Running Trinity #####
 

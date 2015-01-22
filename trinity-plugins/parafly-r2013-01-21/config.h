eda4422c4809cda63e036f5aaf63474d72c9ce89
Brian Haas <brianjohnhaas@gmail.com>
2015-01-22 14:38:33 -0500

    add

---
+++ trinity-plugins/parafly-r2013-01-21/config.h
@@ -0,0 +1,23 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "parafly"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "bug-report@address"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Parafly"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Parafly 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "parafly"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"

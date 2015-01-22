13f941682a8dd04c9dbde5ac54fe68d78ac8e74c
Brian Haas <brianjohnhaas@gmail.com>
2015-01-22 14:20:12 -0500

    add autoconf stuff

---
+++ Inchworm/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "inchworm"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "bug-report@address"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Inchworm"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Inchworm 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "inchworm"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"

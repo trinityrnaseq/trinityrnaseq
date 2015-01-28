e72f72885bd6f6979a6c03d5307a17dfb88e5888
Ben Fulton <befulton@iu.edu>
2015-01-28 13:22:57 -0500

    Update tests to work with latest Trinity version

--- util/support_scripts/tests.py
+++ util/support_scripts/tests.py
@@ -11,12 +11,10 @@ import time
 # module load samtools
 # Trinity
 # Copy the .gz files in sample_data/test_Trinity_Assembly to current directory
-# also unzip the left file using zcat
 # Run using nosetests
-TEMP_FILES = ['both.fa', 'inchworm.K25.L25.fa', 'jellyfish.kmers.fa',
-                  'scaffolding_entries.sam', 'both.fa.read_count',  'jellyfish.kmers.fa',
-              'inchworm.kmer_count','jellyfish.kmers.fa.histo', 'scaffolding_entries.sam',
-              'chrysalis/GraphFromIwormFasta.out']
+MEM_FLAG = "--max_memory 2G"
+TEMP_FILES = ['both.fa', 'bowtie.nameSorted.bam', 'inchworm.K25.L25.fa', 'jellyfish.kmers.fa',
+                  'scaffolding_entries.sam', 'target.fa', 'target.1.ebwt']
 
 
 class TestTrinity(unittest.TestCase):
@@ -33,7 +31,7 @@ class TestTrinity(unittest.TestCase):
 
     def test_sample_data_seq_count(self):
         self.trinity(
-            "Trinity --seqType fq --JM 2G --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --no_cleanup")
+            "Trinity --seqType fq %s --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --no_cleanup" % MEM_FLAG)
         handle = open("trinity_out_dir/Trinity.fasta", "rU")
         seq_count = len([x for x in SeqIO.parse(handle, "fasta")])
         handle.close()
@@ -41,7 +39,7 @@ class TestTrinity(unittest.TestCase):
 
     def test_sample_data_trimmed_and_normalized(self):
         self.trinity(
-            "Trinity --seqType fq --JM 2G --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --trimmomatic --normalize_reads --no_cleanup")
+            "Trinity --seqType fq %s --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --trimmomatic --normalize_reads --no_cleanup" % MEM_FLAG)
         handle = open("trinity_out_dir/Trinity.fasta", "rU")
         seq_count = len([x for x in SeqIO.parse(handle, "fasta")])
         handle.close()
@@ -49,13 +47,13 @@ class TestTrinity(unittest.TestCase):
 
     def test_no_cleanup_leaves_temp_files(self):
         self.trinity(
-            "Trinity --seqType fq --JM 2G --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --no_cleanup")
+            "Trinity --seqType fq %s --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --no_cleanup" % MEM_FLAG)
         for f in TEMP_FILES:
             self.assertTrue(os.path.exists("trinity_out_dir/%s" % f), msg="%s not found with no_cleanup" % f)
 
     def test_cleanup_removes_temp_files(self):
         self.trinity(
-            "Trinity --seqType fq --JM 2G --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --full_cleanup")
+            "Trinity --seqType fq %s --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --full_cleanup" % MEM_FLAG)
         time.sleep(5) # Make sure the system has time to recognize the directory is gone
         self.assertFalse(os.path.exists("trinity_out_dir"), msg="Did full_cleanup but trinity_out_dir exists")
         self.assertTrue(os.path.isfile("trinity_out_dir.Trinity.fasta"),
@@ -68,33 +66,33 @@ class TestTrinity(unittest.TestCase):
             self.assertTrue("Error, with --single reads, the --SS_lib_type can be 'F' or 'R' only." in e.output)
 
     def test_single_end_with_fq(self):
-        self.trinity("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F")
+        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F" % MEM_FLAG)
 
     def test_no_run_chrysalis(self):
-        self.trinity("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F --no_run_chrysalis")
+        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_run_chrysalis" % MEM_FLAG)
         self.assertEquals(0, len(os.listdir('trinity_out_dir/chrysalis')))
 
     def test_no_run_butterfly(self):
-        self.trinity("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F --no_run_butterfly")
+        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_run_butterfly" % MEM_FLAG)
         self.assertTrue(os.path.isfile("trinity_out_dir/chrysalis/butterfly_commands"),
                             msg="butterfly_commands file does not exist")
         self.assertFalse(os.path.isfile("trinity_out_dir/butterfly_commands.completed"),
                             msg="Butterfly appears to have run although no_run_butterfly was specified")
 
     def test_no_run_inchworm(self):
-        self.trinity("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F --no_run_inchworm")
+        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_run_inchworm" % MEM_FLAG)
         self.assertFalse(os.path.isfile("trinity_out_dir/inchworm.K25.L25.fa.finished"),
                             msg="Inchworm appears to have run although no_run_inchworm was specified")
         self.assertTrue(os.path.isfile("trinity_out_dir/jellyfish.kmers.fa"),
                             msg="jellyfish.kmers.fa was not created")
 
     def test_no_bowtie(self):
-        self.trinity("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F --no_bowtie")
+        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_bowtie" % MEM_FLAG)
         self.assertFalse(os.path.isfile("trinity_out_dir/bowtie.nameSorted.bam"),
                             msg="Bowtie appears to have run although no_bowtie was specified")
 
     def test_no_run_quantifygraph(self):
-        self.trinity("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F --no_run_quantifygraph")
+        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_run_quantifygraph" % MEM_FLAG)
         self.assertTrue(os.path.isfile("trinity_out_dir/chrysalis/file_partitioning.ok"),
                             msg="file_partitioning.ok flag file does not exist")
         self.assertTrue(os.path.isfile("trinity_out_dir/chrysalis/quantifyGraph_commands"),
@@ -104,16 +102,17 @@ class TestTrinity(unittest.TestCase):
 
     def test_single_end_with_fa_and_reverse(self):
         self.fq2fa()
-        self.trinity("Trinity --JM 2G --seqType fa --single reads.fa --SS_lib_type R")
+        self.trinity("Trinity %s --seqType fa --single reads.fa --SS_lib_type R" % MEM_FLAG)
 
     def test_output_correctly_changes_dir(self):
         shutil.rmtree('trinity_test', True)
-        self.trinity("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F --output trinity_test")
+        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --output trinity_test" % MEM_FLAG)
         self.assertTrue(os.path.exists("trinity_test"), msg="Changed output directory but it was not created")
         shutil.rmtree('trinity_test', True)
 
     def test_chrysalis_output_correctly_changes_dir(self):
-        self.trinity("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F --chrysalis_output chrysalis_test")
+        self.trinity(
+            "Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --chrysalis_output chrysalis_test" % MEM_FLAG)
         self.assertTrue(os.path.exists("trinity_out_dir/chrysalis_test"),
                         msg="Changed chrysalis output directory but it was not created")
 
@@ -145,10 +144,11 @@ class TestTrinity(unittest.TestCase):
 ### Invalid command line tests
     def test_no_JM_specified_error(self):
         error = self.get_error("Trinity --seqType fq --single reads.left.fq --SS_lib_type F")
-        self.assertTrue("Error, must specify max memory for jellyfish to use, eg.  --JM 10G" in error)
+        self.assertTrue("Error, must specify max memory for jellyfish to use, eg.  --max_memory 10G" in error)
 
     def test_chrysalis_output_must_include_chrysalis(self):
-        error = self.get_error("Trinity --JM 2G --seqType fq --single reads.left.fq --SS_lib_type F --chrysalis_output invalid_name")
+        error = self.get_error(
+            "Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --chrysalis_output invalid_name" % MEM_FLAG)
         self.assertTrue("Error, chrysalis output directory name must include 'chrysalis' in the name" in error)
 
     def test_invalid_option_error(self):

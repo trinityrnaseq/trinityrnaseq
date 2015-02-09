db12d2b4de4939f36f159198115fbd002a1f6a38
Benjamin Fulton <befulton@c11.karst.uits.iu.edu>
2015-02-09 16:57:52 -0500

    Test fixes

--- util/support_scripts/tests.py
+++ util/support_scripts/tests.py
@@ -13,8 +13,7 @@ import time
 # Copy the .gz files in sample_data/test_Trinity_Assembly to current directory
 # Run using nosetests
 MEM_FLAG = "--max_memory 2G"
-TEMP_FILES = ['both.fa', 'bowtie.nameSorted.bam', 'inchworm.K25.L25.fa', 'jellyfish.kmers.fa',
-                  'scaffolding_entries.sam', 'target.fa', 'target.1.ebwt']
+TEMP_FILES = ['both.fa', 'inchworm.K25.L25.fa', 'jellyfish.kmers.fa', 'scaffolding_entries.sam']
 
 
 class TestTrinity(unittest.TestCase):
@@ -35,7 +34,7 @@ class TestTrinity(unittest.TestCase):
         handle = open("trinity_out_dir/Trinity.fasta", "rU")
         seq_count = len([x for x in SeqIO.parse(handle, "fasta")])
         handle.close()
-        self.assertTrue(75 <= seq_count <= 85, msg='Found %s sequences' % seq_count)
+        self.assertTrue(75 <= seq_count <= 90, msg='Found %s sequences' % seq_count)
 
     def test_sample_data_trimmed_and_normalized(self):
         self.trinity(
@@ -72,13 +71,6 @@ class TestTrinity(unittest.TestCase):
         self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_run_chrysalis" % MEM_FLAG)
         self.assertEquals(0, len(os.listdir('trinity_out_dir/chrysalis')))
 
-    def test_no_run_butterfly(self):
-        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_run_butterfly" % MEM_FLAG)
-        self.assertTrue(os.path.isfile("trinity_out_dir/chrysalis/butterfly_commands"),
-                            msg="butterfly_commands file does not exist")
-        self.assertFalse(os.path.isfile("trinity_out_dir/butterfly_commands.completed"),
-                            msg="Butterfly appears to have run although no_run_butterfly was specified")
-
     def test_no_run_inchworm(self):
         self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_run_inchworm" % MEM_FLAG)
         self.assertFalse(os.path.isfile("trinity_out_dir/inchworm.K25.L25.fa.finished"),
@@ -91,14 +83,14 @@ class TestTrinity(unittest.TestCase):
         self.assertFalse(os.path.isfile("trinity_out_dir/bowtie.nameSorted.bam"),
                             msg="Bowtie appears to have run although no_bowtie was specified")
 
-    def test_no_run_quantifygraph(self):
-        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_run_quantifygraph" % MEM_FLAG)
-        self.assertTrue(os.path.isfile("trinity_out_dir/chrysalis/file_partitioning.ok"),
-                            msg="file_partitioning.ok flag file does not exist")
-        self.assertTrue(os.path.isfile("trinity_out_dir/chrysalis/quantifyGraph_commands"),
-                            msg="quantifyGraph_commands file does not exist")
+    def test_no_distributed_trinity_exec(self):
+        self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --no_distributed_trinity_exec" % MEM_FLAG)
+        self.assertTrue(os.path.isfile("trinity_out_dir/inchworm.K25.L25.fa.finished"),
+                            msg="Inchworm did not appear to run with no_distributed_trinity_exec flag")
+        self.assertTrue(os.path.isfile("trinity_out_dir/jellyfish.1.finished"),
+                            msg="Jellyfish did not appear to run with no_distributed_trinity_exec flag")
         self.assertFalse(os.path.isfile("trinity_out_dir/Trinity.fasta"),
-                            msg="Trinity.fasta created with no_run_quantifygraph")
+                            msg="Trinity.fasta created with no_distributed_trinity_exec")
 
     def test_single_end_with_fa_and_reverse(self):
         self.fq2fa()
@@ -109,12 +101,6 @@ class TestTrinity(unittest.TestCase):
         self.trinity("Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --output trinity_test" % MEM_FLAG)
         self.assertTrue(os.path.exists("trinity_test"), msg="Changed output directory but it was not created")
         shutil.rmtree('trinity_test', True)
-
-    def test_chrysalis_output_correctly_changes_dir(self):
-        self.trinity(
-            "Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --chrysalis_output chrysalis_test" % MEM_FLAG)
-        self.assertTrue(os.path.exists("trinity_out_dir/chrysalis_test"),
-                        msg="Changed chrysalis output directory but it was not created")
 
 ### information tests
     def test_cite(self):
@@ -127,8 +113,9 @@ class TestTrinity(unittest.TestCase):
             subprocess.check_output(["Trinity", "--version"])
             self.fail("Version returned 0 errorcode!")
         except subprocess.CalledProcessError as e:
-            self.assertEqual('Trinity version: BLEEDING_EDGE\n', e.output)
+            #self.assertEqual('Trinity version: BLEEDING_EDGE\n', e.output)
             #self.assertEqual('Trinity version: trinityrnaseq_r20140717\n', e.output)
+            self.assertEqual('Trinity version: Trinity_v2.0.2\n', e.output)
 
 
     def test_show_full_usage_info(self):
@@ -145,11 +132,6 @@ class TestTrinity(unittest.TestCase):
     def test_no_JM_specified_error(self):
         error = self.get_error("Trinity --seqType fq --single reads.left.fq --SS_lib_type F")
         self.assertTrue("Error, must specify max memory for jellyfish to use, eg.  --max_memory 10G" in error)
-
-    def test_chrysalis_output_must_include_chrysalis(self):
-        error = self.get_error(
-            "Trinity %s --seqType fq --single reads.left.fq --SS_lib_type F --chrysalis_output invalid_name" % MEM_FLAG)
-        self.assertTrue("Error, chrysalis output directory name must include 'chrysalis' in the name" in error)
 
     def test_invalid_option_error(self):
         error = self.get_error("Trinity --squidward")

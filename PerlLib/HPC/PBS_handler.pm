ea8f611338babab1fb328f9721787735a761a439
Brian Haas <brianjohnhaas@gmail.com>
2015-03-13 08:31:30 -0400

    added space before the -d

--- PerlLib/HPC/PBS_handler.pm
+++ PerlLib/HPC/PBS_handler.pm
@@ -36,7 +36,7 @@ sub submit_job_to_grid {
 
     my $cmd = $self->{config}->{cmd} or confess "Error, need cmd from config file";
     
-    $cmd .= "-d .  -j oe -N $shell_script ";
+    $cmd .= " -d .  -j oe -N $shell_script ";
 
     $cmd .= " $shell_script 2>&1 ";
         

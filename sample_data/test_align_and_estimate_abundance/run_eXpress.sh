0c3a148d6b0c8aac82c24cec99afd03b91ad38a2
Brian Haas <brianjohnhaas@gmail.com>
2015-09-22 19:15:29 -0400

    revamping the testing here

--- sample_data/test_align_and_estimate_abundance/run_eXpress.sh
+++
@@ -1,1 +0,0 @@
-../../util/align_and_estimate_abundance.pl --transcripts Trinity.fasta --seqType fq --left reads.left.fq --right reads.right.fq --gene_trans_map Trinity.fasta.gene_trans_map --aln_method bowtie --est_method eXpress --output_dir eXpress_estimate --SS_lib_type RF

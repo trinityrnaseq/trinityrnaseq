4cd8c7f931ced449574f99f1c3d11270d6ef287a
Brian Haas <brianjohnhaas@gmail.com>
2016-01-04 14:36:58 -0500

    update

--- util/misc/fasta_seq_length.pl
+++ util/misc/fasta_seq_length.pl
@@ -12,6 +12,8 @@ my $usage = "usage: $0 fastaFile\n\n";
 my $file = $ARGV[0] or die $usage;
 
 my $fasta_reader = new Fasta_reader($file);
+
+print join("\t", "fasta_entry", "length") . "\n";
 while (my $seq_obj = $fasta_reader->next()) {
     my $sequence = $seq_obj->get_sequence();
     my $accession = $seq_obj->get_accession();

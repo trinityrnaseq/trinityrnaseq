0b2d1a2e7bb70fb700892c6b285b94cc22903d3d
Brian Haas <brianjohnhaas@gmail.com>
2016-01-24 07:48:19 -0500

    include --normalize_reads in the sample runner

--- sample_data/test_Trinity_Assembly/runMe.sh
+++ sample_data/test_Trinity_Assembly/runMe.sh
@@ -23,7 +23,7 @@ fi
 ##  Run Trinity to Generate Transcriptome Assemblies ##
 #######################################################
 
-../../Trinity --seqType fq --max_memory 2G --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --no_cleanup
+../../Trinity --seqType fq --max_memory 2G --left reads.left.fq.gz,reads2.left.fq.gz --right reads.right.fq.gz,reads2.right.fq.gz --SS_lib_type RF --CPU 4 --no_cleanup --normalize_reads
 
 ##### Done Running Trinity #####
 

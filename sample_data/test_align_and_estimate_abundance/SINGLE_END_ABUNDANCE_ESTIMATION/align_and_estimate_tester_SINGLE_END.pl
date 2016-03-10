facd19cb576be4dff308be175182bb319074ac70
Brian Haas <brianjohnhaas@gmail.com>
2016-03-10 10:10:03 -0500

    start for SE test

---
+++ sample_data/test_align_and_estimate_abundance/SINGLE_END_ABUNDANCE_ESTIMATION/align_and_estimate_tester_SINGLE_END.pl
@@ -0,0 +1,93 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+use File::Basename;
+use lib ("$FindBin::RealBin/../../../PerlLib");
+use Process_cmd;
+
+
+my $usage = "usage: $0 (RSEM|eXpress|kallisto)\n\n";
+
+my $method = $ARGV[0] or die $usage;
+unless ($method =~ /^(RSEM|eXpress|kallisto)$/i) {
+    die $usage;
+}
+
+my $utildir = "$FindBin::RealBin/../../../util";
+
+my $samples_file = "samples.txt";
+my $trinity_fasta = "../../test_DATA/Trinity.fasta";
+
+main: {
+
+    &process_cmd("ln -sf $trinity_fasta .");
+
+    $trinity_fasta = basename($trinity_fasta);
+    
+    my @samples;
+    {
+        open (my $fh, $samples_file) or die $!;
+        while (<$fh>) {
+            chomp;
+            my ($sample_name, $left_fq, $right_fq) = split(/\s+/);
+            $left_fq = &ensure_full_path($left_fq);
+            $right_fq = &ensure_full_path($right_fq);
+            push (@samples, [$sample_name, $left_fq, $right_fq]);
+        }
+        close $fh;
+    }
+
+    my @trans_results;
+    my @gene_results;
+    
+    foreach my $sample (@samples) {
+        my ($sample_name, $left_fq, $right_fq) = @$sample;
+
+        my $cmd = "$utildir/align_and_estimate_abundance.pl --transcripts $trinity_fasta --prep_reference "
+            . " --left $left_fq --right $right_fq --seqType fq --trinity_mode ";
+        
+        my $outdir = "$method-$sample_name";
+
+        if ($method eq 'RSEM') {
+            $cmd .= " --est_method RSEM --output_dir $outdir --aln_method bowtie ";
+            
+            push (@trans_results, "$outdir/RSEM.isoforms.results");
+            push (@gene_results, "$outdir/RSEM.genes.results");
+            
+        }
+        elsif ($method =~ /eXpress/i) {
+            $cmd .= " --est_method eXpress --output_dir $outdir --aln_method bowtie2 ";
+            push (@trans_results, "$outdir/results.xprs");
+            push (@gene_results, "$outdir/results.xprs.genes");
+        }
+        elsif ($method eq 'kallisto') {
+            $cmd .= " --est_method kallisto --output_dir kallisto-$sample_name ";
+            push (@trans_results, "$outdir/abundance.tsv");
+            push (@gene_results, "$outdir/abundance.tsv.genes");
+            
+        }
+        else {
+            # shouldn't ever get here.
+            die "error - method $method not recognized";
+        }
+
+        &process_cmd($cmd);
+
+
+    }
+
+    ## generate matrices.
+    my $cmd = "$utildir/abundance_estimates_to_matrix.pl --est_method $method --out_prefix $method-trans --name_sample_by_basedir @trans_results";
+    &process_cmd($cmd);
+    
+    $cmd = "$utildir/abundance_estimates_to_matrix.pl --est_method $method --out_prefix $method-gene --name_sample_by_basedir @gene_results";
+    &process_cmd($cmd);
+    
+    
+
+    exit(0);
+}
+
---
+++ sample_data/test_align_and_estimate_abundance/SINGLE_END_ABUNDANCE_ESTIMATION/cleanme.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use FindBin;
+
+
+## we delete all files we don't need in this directory. Be careful in case users try running it somewhere else, outside this dir.
+chdir $FindBin::RealBin or die "error, cannot cd to $FindBin::RealBin";
+
+
+
+my @files_to_keep = qw (
+cleanme.pl 
+Makefile
+align_and_estimate_tester_PAIRED_END.pl
+align_and_estimate_tester_SINGLE_END.pl
+samples.txt
+plot_paired_comparisons.Rscript
+);
+
+
+my %keep = map { + $_ => 1 } @files_to_keep;
+
+
+foreach my $file (<*>) {
+	
+	if (-f $file && ! $keep{$file}) {
+		print STDERR "-removing file: $file\n";
+		unlink($file);
+	}
+}
+
+
+`rm -rf ./RSEM-*`;
+`rm -rf ./eXpress-*`;
+`rm -rf ./kallisto-*`;
+
+
+exit(0);

5efe1090f797964303ee4863ea5cf46b9692cceb
Brian Haas <brianjohnhaas@gmail.com>
2016-03-16 20:12:45 -0400

    add

---
+++ sample_data/test_align_and_estimate_abundance/SINGLE_END_ABUNDANCE_ESTIMATION/misc_tests/test_Mouse_ref.sh
@@ -0,0 +1,1 @@
+make TRINITY_FASTA=/seq/regev_genome_portal/RESOURCES/mouse/mm9/Annotations/mm9_ucsc_genomeStudio_genes/mm9_ucsc_genomeStudio_genes.gtf.trans.fa SAMPLES=misc_tests/mouse_ref.samples.txt

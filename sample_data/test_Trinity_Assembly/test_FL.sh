7d47061aea96e6da460028bdfc83dc0c94cc0acd
Brian Haas <brianjohnhaas@gmail.com>
2016-07-03 23:00:41 -0400

    incorporate long read kmer cov info and clustering via chrysalis

--- sample_data/test_Trinity_Assembly/test_FL.sh
+++ sample_data/test_Trinity_Assembly/test_FL.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$TRINITY_HOME/Analysis/FL_reconstruction_analysis/FL_trans_analysis_pipeline.pl --target __indiv_ex_sample_derived/refSeqs.fa --query $*
+$TRINITY_HOME/Analysis/FL_reconstruction_analysis/FL_trans_analysis_pipeline.pl $*

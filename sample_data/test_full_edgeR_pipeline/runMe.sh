e822b06bfc217e06ae6eef8e8cd02f19a66e9e3e
Brian Haas <brianjohnhaas@gmail.com>
2016-11-14 13:16:42 -0500

    update

--- sample_data/test_full_edgeR_pipeline/runMe.sh
+++ sample_data/test_full_edgeR_pipeline/runMe.sh
@@ -9,11 +9,3 @@
 # filter based on fpkm
 ../../util/filter_fasta_by_rsem_values.pl  --rsem_output=ds_rep1.isoforms.results,hs_rep1.isoforms.results,log_rep1.isoforms.results,plat_rep1.isoforms.results --fasta=trinity_out_dir/Trinity.fasta --fpkm_cutoff=0.1 --output=filtered.fasta
 
-# run bowtie alignment to examine read mapping stats:
-../../util/bowtie_PE_separate_then_join.pl  --seqType fq --left reads.ALL.left.fq --right reads.ALL.right.fq --aligner bowtie --output read_content_analysis --target trinity_out_dir/Trinity.fasta -- -p 4 --all --best --strata -m 300
-
-# examine read mapping stats:
-../../util/SAM_nameSorted_to_uniq_count_stats.pl read_content_analysis/read_content_analysis.nameSorted.bam
-
-
-

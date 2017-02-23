682e4562450577c5816a2595dabfacb47f52d556
Brian Haas <brianjohnhaas@gmail.com>
2017-02-23 07:28:16 -0500

    initial work to implement --workdir, also now properly capturing --grid_node_CPU and --grid_node_max_memory param vals

--- sample_data/test_Trinity_Assembly/misc_run_tests/__test_runMe_with_jaccard_clip.sh
+++ sample_data/test_Trinity_Assembly/misc_run_tests/__test_runMe_with_jaccard_clip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ve
 
-../../Trinity --seqType fq --left reads.left.fq.gz --right reads.right.fq.gz --SS_lib_type RF  --CPU 2 --jaccard_clip --max_memory 1G --output __test_trinity_w_jaccard
+../../Trinity --seqType fq --left reads.left.fq.gz --right reads.right.fq.gz --SS_lib_type RF  --CPU 1 --jaccard_clip --max_memory 1G --output __test_trinity_w_jaccard --grid_node_max_memory 2G --grid_node_CPU 2
 
 
 

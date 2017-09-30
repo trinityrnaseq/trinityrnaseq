cc8398a7c32c03718f8871c721f6d724829cad1b
Brian Haas <brianjohnhaas@gmail.com>
2017-09-30 11:29:02 -0400

    use the gzipped fq files

--- sample_data/test_Trinity_Assembly/runMe.sh
+++ sample_data/test_Trinity_Assembly/runMe.sh
@@ -1,23 +1,12 @@
 #!/bin/bash -ve
 
-
-if [ -e reads.right.fq.gz ] && [ ! -e reads.right.fq ]; then
-    gunzip -c reads.right.fq.gz > reads.right.fq
-fi
-
-if [ -e reads.left.fq.gz ] && [ ! -e reads.left.fq ]; then
-    gunzip -c reads.left.fq.gz > reads.left.fq
-fi
-
-
-
 #######################################################
 ##  Run Trinity to Generate Transcriptome Assemblies ##
 #######################################################
 
 ../../Trinity --seqType fq --max_memory 2G \
-              --left reads.left.fq \
-              --right reads.right.fq \
+              --left reads.left.fq.gz \
+              --right reads.right.fq.gz \
               --SS_lib_type RF \
               --CPU 4 
 

0a58b0964308aaff393c09a7ed7992ef15d23570
Brian Haas <brianjohnhaas@gmail.com>
2019-10-20 13:09:21 -0400

    added superreads test

---
+++ sample_data/test_Trinity_Assembly/misc_run_tests/__runMe_wSuperReads.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ve
+
+#######################################################
+##  Run Trinity to Generate Transcriptome Assemblies ##
+#######################################################
+
+if [ -z ${TRINITY_HOME} ]; then
+    echo "Must set env var TRINITY_HOME"
+    exit 1
+fi
+
+
+${TRINITY_HOME}/Trinity --seqType fq --max_memory 2G \
+              --left reads.left.fq.gz \
+              --right reads.right.fq.gz \
+              --SS_lib_type RF \
+              --CPU 4 --output trinity_out_dir_SR --SUPER_READS --no_cleanup
+
+##### Done Running Trinity #####
+
+if [ $* ]; then
+    # check full-length reconstruction stats:
+
+    ${TRINITY_HOME}/util/misc/illustrate_ref_comparison.pl __indiv_ex_sample_derived/refSeqs.fa trinity_out_dir_SR/Trinity.fasta 90
+
+    ./test_FL.sh --query trinity_out_dir_SR/Trinity.fasta --target __indiv_ex_sample_derived/refSeqs.fa --no_reuse
+fi
+

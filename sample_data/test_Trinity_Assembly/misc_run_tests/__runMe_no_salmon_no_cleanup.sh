71b6ce3fc42fbb89b35625da0d5caf7d46f721be
Brian Haas <brianjohnhaas@gmail.com>
2019-11-02 10:13:00 -0400

    butterfly updates wrt pasafly

--- sample_data/test_Trinity_Assembly/misc_run_tests/__runMe_no_salmon_no_cleanup.sh
+++ sample_data/test_Trinity_Assembly/misc_run_tests/__runMe_no_salmon_no_cleanup.sh
@@ -14,7 +14,7 @@ ${TRINITY_HOME}/Trinity --seqType fq --max_memory 2G \
               --left reads.left.fq.gz \
               --right reads.right.fq.gz \
               --SS_lib_type RF \
-              --CPU 4 --no_salmon --no_cleanup
+              --CPU 4 --no_salmon --no_cleanup --verbose
 
 ##### Done Running Trinity #####
 

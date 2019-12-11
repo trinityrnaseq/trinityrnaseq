3f022632268b6a378f84484c4581a399540199a5
brianjohnhaas <brianjohnhaas@gmail.com>
2019-12-11 23:28:32 +0000

    added singularity test runner

---
+++ Docker/run_ext_test_singularity.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+datadir="trinity_ext_sample_data_singularity"
+
+if [ ! -e "$datadir" ]; then
+    git clone https://github.com/trinityrnaseq/trinity_ext_sample_data.git ${datadir}
+fi
+
+
+version=`cat VERSION.txt`
+simg="trinityrnaseq.v${version}.simg"
+
+singularity exec -e -B `pwd`/${datadir}:/${datadir} \
+       ${simg} \
+       bash -c "cd /${datadir} && make"
+

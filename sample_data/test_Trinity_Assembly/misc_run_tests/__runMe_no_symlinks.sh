82dbddf1e553e57b257b469c09b07460b92d24ff
Brian Haas <brianjohnhaas@gmail.com>
2021-08-10 07:12:09 -0400

    added test runner for no symlinks

--- sample_data/test_Trinity_Assembly/misc_run_tests/__runMe_no_symlinks.sh
+++ sample_data/test_Trinity_Assembly/misc_run_tests/__runMe_no_symlinks.sh
@@ -8,8 +8,8 @@ ${TRINITY_HOME}/Trinity --seqType fq --max_memory 2G \
               --left reads.left.fq.gz \
               --right reads.right.fq.gz \
               --SS_lib_type RF \
-              --incl_salmon \
-              --CPU 4 --include_supertranscripts --output trinity_incl_salmon
+              --no_symlink \
+              --CPU 4 --output trinity_no_symlinks
 
 exit 0
 

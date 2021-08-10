dd2d7ac19d3cbea010147ab6ed99e3a451f04f37
Brian Haas <brianjohnhaas@gmail.com>
2021-08-10 10:42:01 -0400

    just providing the final filtered fasta file

--- sample_data/test_Trinity_Assembly/runMe.sh
+++ sample_data/test_Trinity_Assembly/runMe.sh
@@ -24,8 +24,6 @@ if [ $* ]; then
     ${TRINITY_HOME}/util/misc/illustrate_ref_comparison.pl __indiv_ex_sample_derived/refSeqs.fa trinity_out_dir.Trinity.fasta 90
 
     ./test_FL.sh --query  trinity_out_dir.Trinity.fasta --target __indiv_ex_sample_derived/refSeqs.fa --no_reuse
-    ./test_FL.sh --query  trinity_out_dir.Trinity.HQ.fasta --target __indiv_ex_sample_derived/refSeqs.fa --no_reuse
-
 
 fi
 
